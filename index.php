fc5d9b55311abf51a59731f53c071283739d1f27
Robertas Duncia <robertas.duncia@gmail.com>
2020-01-21 08:14:35 +0200

    First file

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <!-- Title which is visible on the browser tab top. -->
+        <title>About me | Robke</title>
+        
+        <!-- Short description to the allmighty Google. -->
+        <meta name="description" content="I am interested in front-end web development projects or jobs!">
+        
+        <!-- Character encoding. -->
+        <meta charset="utf-8"/>
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        
+        <!-- Google fonts. -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap&subset=cyrillic" rel="stylesheet">
+        
+        <!-- CSS linking. -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <!-- Scaling meta. -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Numbers not marked as phone numbers in MS browsers. -->
+        <meta name="format-detection" content="telephone=no"/>
+        
+        <!-- Fav icons. -->
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="img/favicon/site.webmanifest">
+        
+        <!-- jQuery -->
+        
+        <script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+        
+    </head>
+    
+    <body>
+        
+    <!-- Menu part and upper text -->
+        
+    <div class="navMenu">
+        <div class="navInner">
+            <ul>
+                <li><a href="https://www.robert-d.com/index.php">Start (Comic Book Style)</a></li>
+                <li><a href="https://www.robert-d.com/learn.php">JavaScript skills</a></li>
+                <li><a href="https://www.robert-d.com/know.php">HTML, CSS, Bootstrap...</a></li>
+            </ul>   
+        <div class="menuClose">X</div>
+        </div>
+    </div>
+        
+    <div class="hiPart">
+        <h1>Hi, my name is Robke!</h1>
+        <div class="sandwichM">
+            <div><img src="img/menu-new.png"></div>
+        </div>
+    </div>
+        
+    <!-- All content boxes -->
+        
+    <div class="mainBlock"> 
+        
+    <!-- First row of boxes -->    
+        
+        <div class="introPage">
+            <div class="firstLine one">
+                <div class="container partOne">
+                    <div class="textWrapper"><div class="text top">This is a story about my coding skills. Click the menu above or read further...</div></div>
+                    <div class="imageOne"><img src="img/favicon/user.png"></div>
+                </div>
+            </div>
+            
+            <div class="firstLine two">
+                <div class="container partTwo">
+                    <div class="textWrapper">
+                        <div class="text bubble">I was a translator who got interested in web development.</div>
+                        <div class="text triangle"></div>
+                        <div class="text triangleTwo"></div>
+                    </div>
+                    <div class="imageTwo"><img src="img/batman.png"></div>
+                </div>
+            </div>
+            
+            <div class="firstLine three">
+                <div class="container partThree">
+                    <div class="textWrapper"><div class="text top">First I started with plain...</div></div>
+                    <div class="code1Static"><p>HTML and CSS</p></div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Second row of boxes -->  
+        
+        <div class="introPageTwo">
+            <div class="secondLine one">
+                <div class="containerTwo partOne">
+                    <div class="textWrapper"><div class="text top bottom">... it was followed up by advanced CSS + Bootstrap. Now I am learning JavaScript...</div></div>
+                    <div class="textWrapper"><div class="buttonOne"><a href="https://www.robert-d.com/learn.php">Check what I learn</a></div></div>
+                </div>
+            </div>
+            
+            <div class="secondLine two">
+                <div class="containerTwo partTwo">
+                    <div class="textWrapper">
+                        <div class="text bubble middle">For DOM manipulation now I can use JavaScript or jQuery...</div>
+                        <div class="text triangle"></div>
+                        <div class="text triangleTwo"></div>
+                    </div>
+                    <div class="imageTwo"><img src="img/lego.png"></div>
+                </div>
+            </div>
+            
+            <div class="secondLine three">
+                <div class="containerTwo partThree">
+                    <div class="textWrapper">
+                        <div class="imageThree"><img src="img/explosion.png"></div>
+                        <div class="wpText"><p>BOOM! <br>WHAT IS OOP!</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Third row of boxes -->  
+        
+        <div class="introPageThree">
+            <div class="thirdLine one">
+                <div class="containerThree partOne">
+                    <div class="textWrapper"><div class="text top">Functions, if/else statements, arrays... It is time for objects and more skills!</div></div>
+                        <div class="code1"><p>React</p></div>
+                        <div class="code1 version2"><p>AngularJS</p></div>
+                        <div class="code1 version3"><p>JS ES6</p></div>
+                        <div class="code1 version4"><p>Node.js</p></div>
+                        <div class="edgeOnlyText"><p>ES6, Node.js, React</p></div>
+                </div>
+            </div>
+            
+            <div class="thirdLine two">
+                <div class="containerThree partTwo">
+                    <div class="textWrapper">
+                        <div class="text bubble">Click the pill button and see Bootstrap/jQuery examples.</div>
+                        <div class="buttonOne secondB"><a href="https://www.robert-d.com/know.php">Check now</a></div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>   
+        
+    <!-- Bottom text -->  
+    
+    <div class="hiPart"><h1>hello@robert-d.com</h1></div> 
+
+        
+        <script src="script/script.js"></script>
+        
+
+
+    </body>
+</html>
